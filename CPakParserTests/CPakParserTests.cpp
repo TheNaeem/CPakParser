1509bcd0d8317e598a3a82f5902728b1bc89f35d
shade <nkanaan1027@gmail.com>
2022-10-28 19:42:49 -0400

    i hate merge conflicts

--- CPakParserTests/CPakParserTests.cpp
+++ CPakParserTests/CPakParserTests.cpp
@@ -17,16 +17,10 @@ int main()
 	Dataminer::Options::WithLogging(true);
 	Dataminer::Options::WithOodleDecompressor("oo2core_9_win64.dll");
 
-	auto Core = Dataminer("D:\\Fortnite\\FortniteGame\\Content\\Paks");
+	auto Core = Dataminer("C:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Content\\Paks");
 
 	Core.LoadTypeMappings("D:\\N's Stuff\\Output\\.data\\++Fortnite+Release-22.20-CL-22545427-Android_oo.usmap");
 	Core.Initialize();
-	auto t2 = std::chrono::high_resolution_clock::now();
-
-	auto ms_int = duration_cast<std::chrono::milliseconds>(t2 - t1);
-
-	std::chrono::duration<double, std::milli> ms_double = t2 - t1;
-	printf("Initalized in %fms\n", ms_double.count());
 
 	Core.Test("FortniteGame/Content/Athena/Items/Cosmetics/Characters/CID_478_Athena_Commando_F_WorldCup.uasset");
 
